e7494170ddabd7ab2225145fa8f57d64dc0d2b91
Caio Magalhaes <caiomagalhaesdefaria@hotmail.com>
2025-01-12 18:18:32 -0300

    Mudando items da sidebar

--- apps/front-end/src/components/SIdebar/SidebarController.tsx
+++ apps/front-end/src/components/SIdebar/SidebarController.tsx
@@ -1,4 +1,4 @@
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger, Button } from "@app/ui"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger, Button, Text } from "@app/ui"
 import useStore from "../../state"
 import { Link } from "react-router-dom"
 
@@ -12,31 +12,43 @@ export function SidebarController() {
   function getClientItems() {
     return (
       <>
-        <div className="w-full flex items-center">
-          <Button className="btn-black hover:underline text-lg font-extrabold text-white">
-            Solicitar Orçamento
-          </Button>
-        </div>
-        <div className="border-b border-b-[#323b45] mb-5 mt-5" />
-        <Link to="client/budgets" className="hover:underline text-white text-lg font-extrabold" >Orçamentos Recebidos</Link>
-        <div className="border-b border-b-[#323b45] mb-5 mt-5" />
-        <Link to="client/solicitations" className="hover:underline text-white text-lg font-extrabold" >Minhas Solicitações</Link>
-        <div className="border-b border-b-[#323b45] mb-3 mt-5" />
+        <Text as="span" className="font-bold hover:underline text-lg text-white"> Início (Mapa) </Text>
+        <div className="border-b border-b-[#323b45] mt-5" />
+        <Accordion type="single" collapsible className="">
+          <AccordionItem value="item-1">
+            <AccordionTrigger className="mb-2 text-white text-lg font-extrabold">Reparo de celular</AccordionTrigger>
+            <AccordionContent className="hover:text-white">
+              <button className="font-bold hover:text-white hover:underline text-lg text-[#c4c4c4]">
+                Solicitar Orçamento
+              </button>
+            </AccordionContent>
+            <AccordionContent className="">
+              <button className="font-bold hover:text-white hover:underline text-lg text-[#c4c4c4]">
+                Orçamentos Recebidos
+              </button>
+            </AccordionContent>
+            <AccordionContent className="hover:text-white">
+              <button className="font-bold hover:text-white hover:underline text-lg text-[#c4c4c4]">
+                Minhas Solicitações
+              </button>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
         <Accordion type="single" collapsible className="">
           <AccordionItem value="item-1">
             <AccordionTrigger className="mb-2 text-white text-lg font-extrabold">Ajuda</AccordionTrigger>
             <AccordionContent className="hover:text-white">
-              <button className="font-bold">
+              <button className="font-bold hover:text-white hover:underline text-lg text-[#c4c4c4]">
                 Como funciona
               </button>
             </AccordionContent>
             <AccordionContent className="hover:text-white">
-              <button className="font-bold">
+              <button className="font-bold hover:text-white hover:underline text-lg text-[#c4c4c4]">
                 Suporte
               </button>
             </AccordionContent>
             <AccordionContent className="hover:text-white">
-              <button className="font-bold">
+              <button className="font-bold hover:text-white hover:underline text-lg text-[#c4c4c4]">
                 Relatar erro
               </button>
             </AccordionContent>
